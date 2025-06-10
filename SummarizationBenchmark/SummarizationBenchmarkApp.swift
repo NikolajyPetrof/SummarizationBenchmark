9b49926c9c0502f0fb01f9bfd59cc777c9a20188
Nikolay Petrov <npe@aiweapps.com>
2025-06-10 09:31:10 +0300

    Initial Commit

---
+++ SummarizationBenchmark/SummarizationBenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummarizationBenchmarkApp.swift
+//  SummarizationBenchmark
+//
+//  Created by Nikolay Petrov on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SummarizationBenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
